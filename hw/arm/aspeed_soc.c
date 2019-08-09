99371463e877104429af8db8b75fc0cfda3775e9
Cédric Le Goater <clg@kaod.org>
2019-08-09 08:31:08 +0200

    aspeed: add the UART1 serial
    The Aspeed SoCs provide 5 serial communication modules. Let's activate
    UART1 with an extra '-serial' option on the command line :
    
            -chardev socket,id=uart1,host=localhost,port=9004,ipv4,server,nowait \
            -serial chardev:uart1
    
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- hw/arm/aspeed_soc.c
+++ hw/arm/aspeed_soc.c
@@ -515,6 +515,13 @@ static void aspeed_soc_realize(DeviceState *dev, Error **errp)
                        vuart, 38400, serial_hd(1), DEVICE_LITTLE_ENDIAN);
     }
 
+    /* UART1 */
+    if (serial_hd(2)) {
+        qemu_irq uart1 = aspeed_soc_get_irq(s, ASPEED_UART1);
+        serial_mm_init(get_system_memory(), sc->info->memmap[ASPEED_UART1], 2,
+                       uart1, 38400, serial_hd(2), DEVICE_LITTLE_ENDIAN);
+    }
+
     /* I2C */
     object_property_set_bool(OBJECT(&s->i2c), true, "realized", &err);
     if (err) {
