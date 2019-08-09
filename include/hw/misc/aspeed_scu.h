535249c945c8a6a4d2be8ba0bde4ca29191c6208
Joel Stanley <joel@jms.id.au>
2019-08-09 08:31:07 +0200

    aspeed/scu: Add macro for ast2400
    This is similar to the ast2500 and ast2600 versions.
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Cédric Le Goater <clg@kaod.org>

--- include/hw/misc/aspeed_scu.h
+++ include/hw/misc/aspeed_scu.h
@@ -39,6 +39,7 @@ typedef struct AspeedSCUState {
 #define AST2500_A0_SILICON_REV   0x04000303U
 #define AST2500_A1_SILICON_REV   0x04010303U
 
+#define ASPEED_IS_AST2400(si_rev)     ((((si_rev) >> 24) & 0xff) == 0x02)
 #define ASPEED_IS_AST2500(si_rev)     ((((si_rev) >> 24) & 0xff) == 0x04)
 
 extern bool is_supported_silicon_rev(uint32_t silicon_rev);
